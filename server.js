c2bd2c7d5c6b0a791b76e7c4637c2a3df90b4fa3
Niront <nirontmarketplace@gmail.com>
2026-05-15 18:20:06 +0700

    Update server.js

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.post("/generate-image", async (req, res) => {
     const imageResponse = await openai.images.generate({
       model: "gpt-image-1",
       prompt: prompt,
-      size: "1024x1024"
+      size: "512x512"
     });
 
     const imageBase64 = imageResponse.data[0].b64_json;
